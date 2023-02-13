6030415c8ef1cbe9b7ff0e90f7c2b166875cc9da
Emmanuel Mayssat <emmanuel.mayssat@aya.yale.edu>
2023-02-13 12:02:20 -0800

    First update

--- book.js
+++ book.js
@@ -2,13 +2,13 @@ window.BOOK = {
   title: 'Colah the Koala',
   cover: {
     title: 'Colah the Koala',
-    subtitle: 'Colah meets Kiki',
-    author: 'ChatGTP and @tarwin',
-    illustrator: 'SD, SD Midjourney, Dall-e and @tarwin',
+    subtitle: 'Colah meets Kiki and the Palo Alto Midtown AI club',
+    author: 'ChatGTP and @midtown_ai',
+    illustrator: 'SD, SD Midjourney, Dall-e and @midtown_ai',
   },
   pages: [
     { images: ['1-1.webp'], textPosition: 'bottom', text: `Once upon a time in the land down under, there was a sleepy little Koala named Colah. All Colah wanted to do was climb up into her eucalyptus tree and take a nice, long nap.` },
-    { images: ['2-1.webp', '2-2.webp'], textPosition: 'bottom', text: `But every time she closed her eyes, she was kept awake by the loud, annoying laughter of a Kookaburra named Kiki.\n\n"Kiki, please stop that racket!" Colah begged. "I'm trying to sleep!"\n\nBut Kiki just laughed even louder. "Sleep, sleep, who needs sleep?" he cackled. "Let's go exploring instead!"` },
+    { images: ['2-1.webp', '2-2.webp'], textPosition: 'bottom', text: `But every time she closed her eyes, she was kept awake by the loud, excited chatter of a Kookaburra named Kiki.\n\n"Colah, you won't believe what I just discovered!" Kiki exclaimed. "There is a new AI club starting in the forest and I want you to join me!"\n\nBut Kiki just laughed even louder. "Sleep, sleep, who needs sleep?" he cackled. "Let's go exploring instead!"` },
     { images: ['3-1.webp'], textPosition: 'bottom', text: `Colah groaned and rubbed her eyes. She didn't want to explore, she just wanted to sleep. But Kiki wouldn't take no for an answer.\n\n"Come on, Colah, let's go swimming in the billabong!" Kiki suggested.` },
     { images: ['3-2.webp'], textPosition: 'bottom', text: `"No, thank you," Colah said politely. "I prefer to nap in my tree."\n\nBut Kiki wasn't about to give up. He started jumping up and down on Colah's tree, making all the leaves shake and rustle.\n\n"Wake up, wake up, Colah!" Kiki sang. "There's a whole world out there to see!"` },
     { images: ['4-1.webp'], textPosition: 'bottom', text: `Colah couldn't take it anymore. She climbed down from her tree and gave Kiki a stern look.\n\n"Kiki, I'm sorry, but I really need my rest," Colah said firmly. "I'm a Koala, and sleeping is what we do. So please, let me sleep in peace."\n\nKiki finally stopped laughing and looked at Colah with a sheepish grin. "Oh, I'm sorry, Colah," he said. "I didn't realize you were so tired. I'll let you sleep now."` },
