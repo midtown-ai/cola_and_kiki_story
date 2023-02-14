48f06122072a5e22bdbd984fe6d307391a4d91d6
Emmanuel Mayssat <emmanuel.mayssat@aya.yale.edu>
2023-02-13 18:47:11 -0800

    Colah --> Cola

--- book.js
+++ book.js
@@ -1,17 +1,17 @@
 window.BOOK = {
-  title: 'Colah the Koala',
+  title: 'Cola the Koala',
   cover: {
-    title: 'Colah the Koala',
-    subtitle: 'Colah meets Kiki and the Palo Alto Midtown AI club',
+    title: 'Cola the Koala',
+    subtitle: 'Cola, Kiki, and the AI club',
     author: 'ChatGTP and @midtown_ai',
     illustrator: 'SD, SD Midjourney, Dall-e and @midtown_ai',
   },
   pages: [
-    { images: ['1-1.webp'], textPosition: 'bottom', text: `Once upon a time in the land down under, there was a sleepy little Koala named Colah. All Colah wanted to do was climb up into her eucalyptus tree and take a nice, long nap.` },
-    { images: ['2-1.webp', '2-2.webp'], textPosition: 'bottom', text: `But every time she closed her eyes, she was kept awake by the loud, excited chatter of a Kookaburra named Kiki.\n\n"Colah, you won't believe what I just discovered!" Kiki exclaimed. "There is a new AI club starting in the forest, and I want you to join me!"` },
-    { images: ['3-1.webp'], textPosition: 'bottom', text: `Colah groaned and rubbed her eyes. She wasn't interested in any club. She just wanted to sleep. But Kiki wouldn't take no for an answer.\n\n"Come on, Colah, let's go there together. It will be so much fun!" Kiki suggested. "We will learn about AI and all the amazing things it can do."` },
-    { images: ['3-2.webp'], textPosition: 'bottom', text: `"No, thank you," Colah said politely. "I prefer to nap in my tree."\n\nBut Kiki wasn't about to give up. He started jumping up and down on Colah's tree, making all the leaves shake and rustle.\n\n"Wake up, wake up, Colah!" Kiki sang. "Don't you want to expand your knowledge and keep up with the latest technology?"` },
-    { images: ['4-1.webp'], textPosition: 'bottom', text: `Colah couldn't take it anymore. She climbed down from her tree and gave Kiki a stern look.\n\n"Kiki, I'm sorry, but I really need my rest," Colah said firmly. "I'm a Koala, and sleeping is what we do. So please, please, please... let me sleep in peace."\n\nKiki finally gave up and nodded. "Okay, Colah. I'll let you sleep" he said with a grin. "But just know, you will be missing our on a lot of fun and exciting opportunities"` },
-    { images: ['5-1.webp'], textPosition: 'bottom', text: `Colah smiled and climbed back up into her tree. She settled into her cozy eucalyptus leaves and closed her eyes.\n\nAnd finally, after all the noise and excitement, Colah was able to drift off into a peaceful, dreamy sleep.` },
+    { images: ['1-1.webp'], textPosition: 'bottom', text: `Once upon a time in the land down under, there was a sleepy little Koala named Cola. All Cola wanted to do was climb up into her eucalyptus tree and take a nice, long nap.` },
+    { images: ['2-1.webp', '2-2.webp'], textPosition: 'bottom', text: `But every time she closed her eyes, she was kept awake by the loud, excited chatter of a Kookaburra named Kiki.\n\n"Cola, you won't believe what I just discovered!" Kiki exclaimed. "There is a new AI club starting in the forest, and I want you to join me!"` },
+    { images: ['3-1.webp'], textPosition: 'bottom', text: `Cola groaned and rubbed her eyes. She wasn't interested in any club. She just wanted to sleep. But Kiki wouldn't take no for an answer.\n\n"Come on, Cola, let's go there together. It will be so much fun!" Kiki suggested. "We will learn about AI and all the amazing things it can do."` },
+    { images: ['3-2.webp'], textPosition: 'bottom', text: `"No, thank you," Cola said politely. "I prefer to nap in my tree."\n\nBut Kiki wasn't about to give up. He started jumping up and down on Cola's tree, making all the leaves shake and rustle.\n\n"Wake up, wake up, Cola!" Kiki sang. "Don't you want to expand your knowledge and keep up with the latest technology?"` },
+    { images: ['4-1.webp'], textPosition: 'bottom', text: `Cola couldn't take it anymore. She climbed down from her tree and gave Kiki a stern look.\n\n"Kiki, I'm sorry, but I really need my rest," Cola said firmly. "I'm a Koala, and sleeping is what we do. So please, please, please... let me sleep in peace."\n\nKiki finally gave up and nodded. "Okay, Cola. I'll let you sleep" he said with a grin. "But just know, you will be missing our on a lot of fun and exciting opportunities"` },
+    { images: ['5-1.webp'], textPosition: 'bottom', text: `Cola smiled and climbed back up into her tree. She settled into her cozy eucalyptus leaves and closed her eyes.\n\nAnd finally, after all the noise and excitement, Cola was able to drift off into a peaceful, dreamy sleep.` },
   ]
 }
